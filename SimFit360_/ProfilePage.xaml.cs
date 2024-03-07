91f2656d2acc68157a8ed73133d8f665addeebdd
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-07 12:01:08 +0100

    Update ProfilePage.xaml.cs
    baackend of the page, should load in the correct data for the logged in user.

--- SimFit360_/ProfilePage.xaml.cs
+++ SimFit360_/ProfilePage.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using SimFit360.Model;
+using Windows.Media.Streaming.Adaptive;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,12 +24,23 @@ namespace SimFit360_
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class ProfilePage : Page
-    {
+    { 
         private static User LoggedInUser { get; }   
 
         public ProfilePage()
         {
             this.InitializeComponent();
+
+            using var db = new AppDbContext();
+
+            var currentUser = LoggedInUser; 
+
+            ProfileListView.ItemsSource = currentUser;
+        }
+
+        private void StatisticsNav_Click(object sender, RoutedEventArgs e)
+        {
+            //Frame.Navigate(typeof(StatisticsPage));
         }
     }
 }
